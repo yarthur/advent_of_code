6eda7cda643e583cf9a4473022bd74c553c329ca
John Arthur <john@jtarthur.net>
2017-12-08 13:22:26 -0600

    Complete the excerises for Day 8.

---
+++ 08/index.mjs
@@ -0,0 +1,4 @@
+import instructions from './instructions.mjs';
+import part1 from './part1.mjs';
+
+part1(instructions);
---
+++ 08/parseInstructions.mjs
@@ -0,0 +1,19 @@
+var mapInstructionString = function (instructionString) {
+	var instructionParts = /^([a-z]+) (inc|dec) (-?\d+) if ([a-z]+ [<>=!]{1,2} -?\d*)$/.exec(instructionString),
+		parsedInstruction = {
+			register: instructionParts[1],
+			modification: instructionParts[2],
+			modifier: Number(instructionParts[3]),
+			condition: instructionParts[4]
+		};
+
+	return parsedInstruction;
+};
+
+var parseInstructions = function (instructionString) {
+	var instructions = instructionString.split('\n');
+
+	return instructions.map(mapInstructionString);
+}
+
+export default parseInstructions;
