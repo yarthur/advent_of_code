843b3f0e149a24eea878684dedb5a46b5d808c49
John Arthur <john@jtarthur.net>
2017-12-12 12:07:21 -0600

    Solve for Day 12.

---
+++ 12/groupPrograms.mjs
@@ -0,0 +1,23 @@
+var groupPrograms = function (programs, pipesSeed = null) {
+	var programPipes = new Set(pipesSeed),
+		pipesCountStart = programPipes.size,
+		pipesCountEnd = 0;
+
+	while (pipesCountStart !== pipesCountEnd) {
+		pipesCountStart = programPipes.size;
+
+		for (let program in programs) {
+			if (programPipes.has(program)) {
+				programs[program].forEach(function (receiver) {
+					programPipes.add(receiver);
+				});
+			}
+		}
+
+		pipesCountEnd = programPipes.size
+	}
+
+	return programPipes;
+};
+
+export default groupPrograms;
