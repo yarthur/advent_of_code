c495ad0dd4734a4b74bd2872c7b11eda5a3b6a7c
John Arthur <john@jtarthur.net>
2017-12-12 12:16:10 -0600

    Solve for Day 5.

---
+++ 05/processInput.mjs
@@ -0,0 +1,10 @@
+var processInput = function (input) {
+	var splitInput = input.split('\n'),
+		processedInput = splitInput.map(function (val) {
+			return Number(val);
+		});
+
+	return processedInput;
+};
+
+export default processInput;
